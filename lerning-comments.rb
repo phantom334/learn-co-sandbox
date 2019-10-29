c40bdbd05a769e390e783e8c97fc5a0ddb551fdb
Emmory Leach <leach.emmory@icloud.com>
2019-10-29 21:29:21 +0000

    Automatically backed up by Learn

---
+++ lerning-comments.rb
@@ -0,0 +1,10 @@
+# Perform a constant expression evaluation
+3
+# Assign constant 3 to bare word variable triangle_sides
+'triangle_sides'
+
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
